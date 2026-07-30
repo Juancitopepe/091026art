7530f6d5781f4afcf776f67045054e8061d50bc3
Juancitopepe <1pepitojuanvladsc@gmail.com>
2026-07-30 13:48:49 -0300

    Fix indentation and formatting in main.py

--- main.py
+++ main.py
@@ -26,36 +26,36 @@ with open(NOMBRE_SALIDA, "w") as archivo:
 
     for y in range(60):
 
-    fila = []
+        fila = []
 
-    for x in range(60):
+        for x in range(60):
 
-        r, g, b = imagen.getpixel((x, y))
+            r, g, b = imagen.getpixel((x, y))
 
-        suma = r + g + b
+            suma = r + g + b
 
-        if suma <= 95:
-            valor = 0
-        elif suma <= 191:
-            valor = 1
-        elif suma <= 287:
-            valor = 2
-        elif suma <= 382:
-            valor = 3
-        elif suma <= 478:
-            valor = 4
-        elif suma <= 574:
-            valor = 5
-        elif suma <= 670:
-            valor = 6
-        else:
-            valor = 7
+            if suma <= 95:
+                valor = 0
+            elif suma <= 191:
+                valor = 1
+            elif suma <= 287:
+                valor = 2
+            elif suma <= 382:
+                valor = 3
+            elif suma <= 478:
+                valor = 4
+            elif suma <= 574:
+                valor = 5
+            elif suma <= 670:
+                valor = 6
+            else:
+                valor = 7
 
-        fila.append(str(valor))
+            fila.append(str(valor))
 
-    archivo.write(" ".join(fila))
+        archivo.write(" ".join(fila))
 
-    if y != 59:
-        archivo.write("\n")
+        if y != 59:
+            archivo.write("\n")
 
 print("✅ Archivo generado correctamente:", NOMBRE_SALIDA)
